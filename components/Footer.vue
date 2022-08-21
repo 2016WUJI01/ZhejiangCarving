fbd9d59f24c01b2e29f99b38bd4ed1b198ed97b5
WYLQH <2971387502@qq.com>
2022-08-21 17:07:13 +0800

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     <div class="container top">
       <div class="footer-top">
-        <div class="left">
+        <div class="content left">
           <div class="title">Visit</div>
           <p>Bone Inlay Wood Carving</p>
           <p>DongYang Wood CarvingVisit</p>
@@ -10,26 +10,27 @@
           <p>Gold Lacquer Painted Wood Carving</p>
           <p>Qingtian Stone Sculpture</p>
         </div>
-        <div class="mid">
+        <div class="content mid">
           <div class="title">Resources</div>
           <p>Collection Donation</p>
           <p>Masterpieces by Famous Artists</p>
           <p>Circulating Collection</p>
           <p>Lost and Found</p>
         </div>
-        <div class="right">
+        <div class="content right">
           <div class="title">Our work</div>
           <p>Contact us</p>
           <p>Retrive your password</p>
         </div>
       </div>
+
       <div class="footer-bottom">
-        <div class="left">
+        <div class="content left">
           <div class="title">Commercial</div>
           <p>Launch</p>
           <p>Offline Collection Festival</p>
         </div>
-        <div class="mid">
+        <div class="content mid">
           <div class="title">Research</div>
           <p>Expert interpretation</p>
           <p>Visit Notes Report</p>
@@ -37,7 +38,6 @@
         </div>
       </div>
     </div>
-    <hr class="hr" />
     <div class="container bottom">
       <div>PRIVACY POLICY</div>
       <div>COOKIES</div>
@@ -56,18 +56,27 @@ export default {}
 @import '~/assets/style/common.less';
 .footer {
   background-color: #d8d2c3;
-  height: 179px;
+  height: 576px;
+  position: relative;
 }
 
 .top {
-  height: 132px;
-  padding-top: 30px;
+  position: absolute;
+  // left: 9.25%;
+  // right: 19.53%;
+  // top: 9.55%;
+  // bottom: 30.73%;
   .footer-top {
-    float: left;
-    .left {
+    height: 290px;
+    .content {
+      width: 400px;
+      display: block;
+      float: left;
+      top: 55px;
+      margin-right: 126px;
       .title {
         font-weight: 700;
-        font-size: 55px;
+        font-size: 36px;
         font-family: Big Shoulders Display;
         line-height: 43.09px;
       }
@@ -78,37 +87,27 @@ export default {}
         line-height: 27px;
       }
     }
-    .mid {
-      .title {
-        font-weight: 800;
-        font-size: 55px;
-        font-family: Big Shoulders Display;
-        line-height: 55px;
-      }
-      p {
-        font-size: 16px;
-        font-weight: 500;
-        font-family: Roboto Slab;
-        line-height: 16px;
-      }
-    }
-    .right {
-      .title {
-        font-weight: 800;
-        font-size: 55px;
-        font-family: Big Shoulders Display;
-        line-height: 55px;
-      }
-      p {
-        font-size: 16px;
-        font-weight: 500;
-        font-family: Roboto Slab;
-        line-height: 16px;
-      }
-    }
   }
   .footer-bottom {
-    float: right;
+    height: 206px;
+    .content {
+      width: 400px;
+      display: block;
+      float: left;
+      margin-right: 126px;
+      .title {
+        font-weight: 700;
+        font-size: 36px;
+        font-family: Big Shoulders Display;
+        line-height: 43.09px;
+      }
+      p {
+        font-size: 18px;
+        font-weight: 400;
+        font-family: Roboto Slab;
+        line-height: 27px;
+      }
+    }
   }
 }
 
@@ -117,5 +116,9 @@ export default {}
 }
 
 .bottom {
+  width: 100%;
+  height: 80px;
+  margin-left: 0;
+  background-color: rgba(0, 0, 0, 0.1);
 }
 </style>
