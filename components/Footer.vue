336b3612e9bda7bed00023a7c54fc87d14c778d8
吴嘉顺 <870435170@qq.com>
2022-08-27 15:24:15 +0800

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,135 +1,135 @@
 <template>
   <div class="footer">
-    <div class="container top">
-      <div class="footer-top">
-        <div class="content left">
-          <div class="title font-BigShouldersDisplay">Visit</div>
-          <p>Bone Inlay Wood Carving</p>
-          <p>DongYang Wood CarvingVisit</p>
-          <p>Mud Gold acquerware</p>
-          <p>Gold Lacquer Painted Wood Carving</p>
-          <p>Qingtian Stone Sculpture</p>
-        </div>
-        <div class="content mid">
-          <div class="title font-BigShouldersDisplay">Resources</div>
-          <p>Collection Donation</p>
-          <p>Masterpieces by Famous Artists</p>
-          <p>Circulating Collection</p>
-          <p>Lost and Found</p>
-        </div>
-        <div class="content right">
-          <div class="title font-BigShouldersDisplay">Our work</div>
-          <p>Contact us</p>
-          <p>Retrive your password</p>
+    <div class="container">
+      <div class="">
+        <div class="">
+          <div class="">
+            <div>
+              <div class="title">Visit</div>
+              <p>Bone Inlay Wood Carving</p>
+              <p>DongYang Wood CarvingVisit</p>
+              <p>Mud Gold acquerware</p>
+              <p>Gold Lacquer Painted Wood Carving</p>
+              <p>Qingtian Stone Sculpture</p>
+            </div>
+            <div>
+              <div class="title">Commercial</div>
+              <p>Launch</p>
+              <p>Offline Collection Festival</p>
+            </div>
+          </div>
+          <div class="">
+            <div>
+              <div class="title">Resources</div>
+              <p>Collection Donation</p>
+              <p>Masterpieces by Famous Artists</p>
+              <p>Circulating Collection</p>
+              <p>Lost and Found</p>
+            </div>
+            <div>
+              <div class="title">Research</div>
+              <p>Expert interpretation</p>
+              <p>Visit Notes Report</p>
+              <p>Lecture Forum</p>
+            </div>
+          </div>
+          <div class="">
+            <div class="title">Our work</div>
+            <p>Contact us</p>
+            <p>Retrive your password</p>
+          </div>
         </div>
       </div>
-
-      <div class="footer-bottom">
-        <div class="content left">
-          <div class="title font-BigShouldersDisplay">Commercial</div>
-          <p>Launch</p>
-          <p>Offline Collection Festival</p>
+      <div class="flex__inlineCenter">
+        <div class="flex__inlineCenter float-left">
+          <div>PRIVACY POLICY</div>
+          <div>COOKIES</div>
+          <div>ACCESSIBILITY STATEMENT</div>
+          <div>TERM OF USE</div>
         </div>
-        <div class="content mid">
-          <div class="title font-BigShouldersDisplay">Research</div>
-          <p>Expert interpretation</p>
-          <p>Visit Notes Report</p>
-          <p>Lecture Forum</p>
+        <div class="flex__inlineCenter float-right">
+          <div class="logo">@2022浙江雕刻艺术</div>
         </div>
       </div>
-    </div>
-    <div class="container bottom">
-      <div class="indent" type="Navigation Bar - Button">PRIVACY POLICY</div>
-      <div>COOKIES</div>
-      <div>ACCESSIBILITY STATEMENT</div>
-      <div>TERM OF USE</div>
-      <div class="logo">@2022浙江雕刻艺术</div>
     </div>
   </div>
 </template>
 
-<script>
-export default {}
-</script>
-
 <style lang="less">
 @import '~/assets/style/common.less';
 .footer {
-  margin: 0;
-  padding: 0;
   background-color: #d8d2c3;
-  height: 606px;
-  position: relative;
 }
 
-.top {
-  height: 496px;
-  .footer-top {
-    height: 290px;
-    .content {
-      width: 400px;
-      display: block;
-      float: left;
-      margin-right: 126px;
-      .title {
-        font-weight: 700;
-        font-size: 36px;
-        line-height: 43.09px;
-      }
-      p {
-        font-size: 18px;
-        font-weight: 400;
-        font-family: Roboto Slab;
-        line-height: 27px;
-      }
-    }
-  }
-  .footer-bottom {
-    height: 206px;
-    .content {
-      width: 400px;
-      display: block;
-      float: left;
-      margin-right: 126px;
-      .title {
-        font-weight: 700;
-        font-size: 36px;
-        line-height: 43.09px;
-      }
-      p {
-        font-size: 18px;
-        font-weight: 400;
-        font-family: Roboto Slab;
-        line-height: 27px;
-      }
-    }
-  }
-}
+// .top {
+//   height: 496px;
+//   .footer-top {
+//     height: 290px;
+//     .content {
+//       width: 400px;
+//       display: block;
+//       float: left;
+//       margin-right: 126px;
+//       .title {
+//         font-weight: 700;
+//         font-size: 36px;
+//         line-height: 43.09px;
+//         .font-BigShouldersDisplay;
+//       }
+//       p {
+//         font-size: 18px;
+//         font-weight: 400;
+//         font-family: Roboto Slab;
+//         line-height: 27px;
+//       }
+//     }
+//   }
+//   .footer-bottom {
+//     height: 206px;
+//     .content {
+//       width: 400px;
+//       display: block;
+//       float: left;
+//       margin-right: 126px;
+//       .title {
+//         font-weight: 700;
+//         font-size: 36px;
+//         line-height: 43.09px;
+//       }
+//       p {
+//         font-size: 18px;
+//         font-weight: 400;
+//         font-family: Roboto Slab;
+//         line-height: 27px;
+//       }
+//     }
+//   }
+// }
 
-.bottom {
-  height: 80px;
-  width: 100%;
-  justify-content: left;
-  background-color: rgba(0, 0, 0, 0.1);
-  margin: 0;
-  .logo {
-    font-size: Roboto Slab;
-    weight: 400;
-    size: 18px;
-    line-height: 23.74px;
-    margin-left: 700px;
-  }
-  .indent {
-    margin-left: 136px;
-  }
-  div {
-    font-family: Roboto;
-    text-transform: uppercase;
-    font-style: normal;
-    weight: 700;
-    size: 16px;
-    line-height: 18.75px;
-    margin-left: 30px;
-  }
-}
+// .bottom {
+//   height: 80px;
+//   width: 100%;
+//   justify-content: left;
+//   background-color: rgba(0, 0, 0, 0.1);
+//   margin: 0;
+//   .logo {
+//     font-size: Roboto Slab;
+//     weight: 400;
+//     size: 18px;
+//     line-height: 23.74px;
+//     margin-left: 700px;
+//   }
+//   .indent {
+//     margin-left: 136px;
+//   }
+//   div {
+//     font-family: Roboto;
+//     text-transform: uppercase;
+//     font-style: normal;
+//     weight: 700;
+//     size: 16px;
+//     line-height: 18.75px;
+//     margin-left: 30px;
+//   }
+// }
 </style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="header">
-    <div class="container top">
-      <div class="header-left float-left">
-        <div class="title font-BigShouldersStencilDisplay">
-          The Art of Carving
+    <div class="container">
+      <div class="top">
+        <div class="top-left">
+          <div class="title">The Art of Carving</div>
+          <div class="sub-title">
+            in Zhejiang Overseas Interactive Exhibition
+          </div>
         </div>
-        <div class="sub-title font-RobotoSlab">
-          in Zhejiang Overseas Interactive Exhibition
+        <div class="top-right">
+          <div class="top-right-item">
+            <img src="~/assets/images/icon_search.png" class="header-icon" />
+            Search
+          </div>
+          <div class="top-right-item">
+            <img src="~/assets/images/icon_donate.png" class="header-icon" />
+            Donate
+          </div>
+          <div class="top-right-item">Sign in</div>
         </div>
       </div>
-      <div class="header-right flex__container float-right">
-        <div class="header-right-item flex__container">
-          <img src="~/assets/images/icon_search.png" class="header-icon" />
-          Search
+      <hr class="hr" />
+      <div class="bottom">
+        <div class="bottom-left">
+          <div class="bottom-left-item">Home</div>
+          <div class="bottom-left-item">>></div>
+          <div class="bottom-left-item">Carving Bone Inlay Wood Carving</div>
         </div>
-        <div class="header-right-item flex__container">
-          <img src="~/assets/images/icon_donate.png" class="header-icon" />
-          Donate
+        <div class="bottom-right">
+          <div class="bottom-right-item">Visit</div>
+          <div class="bottom-right-item">Craft and Collectibles</div>
+          <div class="bottom-right-item">Artists</div>
+          <div class="bottom-right-item">Support us</div>
         </div>
-        <div class="header-right-item flex__container">Sign in</div>
-      </div>
-    </div>
-    <hr class="hr" />
-    <div class="container bottom flex__container">
-      <div class="bottom-left flex__container float-left">
-        <div class="bottom-left-item">Home</div>
-        <div class="bottom-left-item">>></div>
-        <div class="bottom-left-item">Carving Bone Inlay Wood Carving</div>
-      </div>
-      <div class="bottom-right flex__container float-right">
-        <div class="bottom-right-item">Visit</div>
-        <div class="bottom-right-item">Craft and Collectibles</div>
-        <div class="bottom-right-item">Artists</div>
-        <div class="bottom-right-item">Support us</div>
       </div>
     </div>
   </div>
@@ -52,30 +52,39 @@ export default {}
 .top {
   height: 100px;
   padding-top: 30px;
-  .header-left {
+  .top-left {
+    .float-left;
+
     .title {
       font-weight: 800;
       font-size: 55px;
       line-height: 55px;
+      .font-BigShouldersDisplay;
     }
 
     .sub-title {
       font-size: 18px;
       font-weight: 500;
       line-height: 18px;
+      .font-RobotoSlab;
     }
   }
 
-  .header-right {
-    .header-right-item {
-      .header-icon {
-        height: 38px;
-        width: 38px;
-      }
+  .top-right {
+    .float-right;
+    .flex__inlineCenter;
+
+    .top-right-item {
+      .flex__inlineCenter;
 
       margin-left: 10px;
       height: 40px;
       width: 100px;
+
+      .header-icon {
+        height: 38px;
+        width: 38px;
+      }
     }
   }
 }
@@ -85,8 +94,13 @@ export default {}
 }
 
 .bottom {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
+  .bottom-left {
+    .float-left;
+    .flex__inline;
+  }
+  .bottom-right {
+    .float-right;
+    .flex__inline;
+  }
 }
 </style>
