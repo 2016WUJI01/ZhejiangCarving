b4b9c69fcb262c1f706b986279dc0015f043c4b9
2016WUJI01 <jiashunwu2001@163.com>
2022-08-30 21:18:45 +0800

    update

--- pages/HomePage/index.vue
+++ pages/HomePage/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div class="banner">
+    <div class="homePage-banner">
       <img src="~/assets/images/Homepage/1.png" />
-      <div>
+      <div class="homePage-banner-textArea">
         <div>Welcome to Zhejiang Wood Carving</div>
         <div>
           Discover the beauty of Ningbo's heritage and experience the essence of
@@ -12,9 +12,11 @@
       </div>
     </div>
     <div class="container">
-      <div>
-        <div>Discover the cultural charm of Zhejiang carving art</div>
-        <div>
+      <div class="homePage-introduction">
+        <div class="float-left w-600px homePage-introduction-left">
+          Discover the cultural charm of Zhejiang carving art
+        </div>
+        <div class="float-right">
           <div>Feel free to visit and browse</div>
           <div>
             <div>Time: 00:00~24:00</div>
@@ -22,7 +24,7 @@
           </div>
         </div>
       </div>
-      <div>
+      <div class="row">
         <div>Carving Art Categories</div>
         <div></div>
       </div>
@@ -71,10 +73,29 @@
 <style lang="less">
 @import '~/assets/style/common.less';
 
-.banner {
+.homePage-banner {
   width: 100%;
+
   img {
     width: 100%;
+  }
+
+  .homePage-banner-textArea {
+    background: rgba(#5c4e41, 0.85);
+    width: 25%;
+    height: 18%;
+  }
+}
+
+.homePage-introduction {
+  .homePage-introduction-left {
+    font-family: 'Big Shoulders Display';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 64px;
+    line-height: 77px;
+    letter-spacing: -0.02em;
+    color: #454033;
   }
 }
 </style>
