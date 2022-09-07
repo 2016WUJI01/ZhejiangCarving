9eaade52a02e9fa073aabc88e7d39a3f9011ccd9
2016WUJI01 <jiashunwu2001@163.com>
2022-09-07 13:26:33 +0800

    updaye

--- pages/Craft/gold.vue
+++ pages/Craft/gold.vue
@@ -1,9 +1,213 @@
 <template>
-  <div>gold</div>
-  <div></div>
+  <div><img src="~/assets/images/朱金漆木雕/1.png" class="w-full" /></div>
+  <div class="gold-row gold-row-background">
+    <div class="container craft-row">
+      <div class="craft-row-item gold-headline-left">
+        Gold Lacquer Painted Wood Carving
+      </div>
+      <div class="craft-row-item gold-headline-right">
+        A traditional craft art of applying gold and lacquer to wood carvings
+      </div>
+    </div>
+  </div>
+  <div class="gold-row">
+    <div class="container craft-row">
+      <div class="craft-row-item">
+        <img src="~/assets/images/朱金漆木雕/2.png" class="w-full" />
+      </div>
+      <div class="craft-row-item">
+        <div class="gold-row-title">A brief description is as follows</div>
+        <div class="gold-row-body">
+          The main technique is to apply gold and lacquer to the wood carving,
+          which is centred in Ningbo, Zhejiang Province, extending to Cixi,
+          Yuyao, Fenghua, Xiangshan, Ninghai, Zhenhai, Yinzhou and other places.
+          The real meaning of Ningbo Zhujin wood carving originated from the Han
+          Dynasty, when the art of carving and lacquering and gold leaf appliqué
+          was applied to buildings and decorative wood carvings in the Tang
+          Dynasty, and gradually shifted to furniture and screens after the Ming
+          and Qing Dynasties, due to the restrictions of the architectural
+          system.
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="gold-row">
+    <div class="container craft-row">
+      <div class="craft-row-item">
+        <div class="gold-row-title">Uses in the long run of time</div>
+        <div class="gold-row-body">
+          According to the History of Japan-China Cultural Exchanges by Japanese
+          scholar Taiko Kimiya, a lacquered gold carved box was given as a gift
+          to Japan by the government of Mingzhou during the Southern Song
+          Dynasty. Legend has it that when the Tang Dynasty monk Jianzhen and
+          his disciples built the Zhaoti Temple in Japan, they made reference to
+          the wood carving decoration of the Ashoka Temple in Ningbo, where the
+          decorative styles of the Hall of Sutra and the Hall of Relics were
+          very similar to the domestic vermilion and gold wood carvings. "It was
+          used in everyday life, from decoration on a regular basis, to the
+          decoration of temple buildings and the beautification of residential
+          houses, and especially in wedding ceremonies, where it was used in
+          beds and palanquins.
+        </div>
+      </div>
+      <div class="craft-row-item">
+        <img src="~/assets/images/朱金漆木雕/3.png" class="w-full" />
+      </div>
+    </div>
+  </div>
+  <div class="gold-row gold-row-background">
+    <div class="container craft-row">
+      <div class="craft-row-item">
+        <img src="~/assets/images/朱金漆木雕/4.png" class="w-full" />
+      </div>
+      <div class="craft-row-item">
+        <div class="gold-row-title">Preservation conditions</div>
+        <div class="gold-row-body">
+          Ningbo is located on the shores of the East China Sea and in the
+          southeastern corner of the Yangtze River Delta, on the Ning-Shao
+          Plain, at a moderate latitude, in the northern subtropical monsoon
+          climate zone, mild and humid. In the 1970s archaeologists discovered
+          wooden lacquer bowls and lacquer cylinders at the primitive social
+          site of Hemudu in Yuyao, Zhejiang.
+          <br />
+          The General History of Zhejiang records that "During the Xuande period
+          of the Ming Dynasty (1426~1435), Ningbo was famous for its clay and
+          gold coloured lacquer mistysia and gold lacquer ware ......." It is
+          clear that Ningbo has always produced lacquerware products in
+          abundance. Lacquerware is best preserved in a place with slightly
+          higher ambient humidity and at a temperature of around 15°C to 35°C.
+          This is because if the air is too dry, the lacquer is prone to
+          fracture; too much humidity makes it prone to deformation and paint
+          stripping. The climatic conditions in Ningbo have provided objective
+          conditions for the development of Zhujin wood carving.
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="gold-row">
+    <div class="container craft-row">
+      <div class="gold-row-title">
+        The household name of the "Thousand Worker's Bed" and the "Ten Thousand
+        Worker's Sedan Chair"
+      </div>
+      <div class="gold-row-body">
+        "The carvings are the result of the hard work and wisdom of Ningbo's
+        Ming and Qing dynasty wood carvers, and are now part of the intangible
+        cultural heritage, representing Ningbo's folklore and an important
+        vehicle for local culture.
+      </div>
+    </div>
+  </div>
+  <div class="gold-row">
+    <div class="container craft-row">
+      <div class="craft-row-item">
+        <div class="gold-row-body">
+          "The bed is also known as the 'Ningbo Carved Flower Bed', measuring
+          212 cm in length and 120 cm in width; the step is 28 cm in depth and
+          28 cm in height. The bed has a roll-topped roof and a footstep
+          underneath, with a portico consisting of a carved pillar frame, a
+          hanging drop, and a leaning eave with a flower cover in front. A small
+          cupboard with two buckets and two doors is placed to the right of the
+          portico, with a bell, hatbox, vase, mirror box, tea set and lampstand;
+          a toilet box is placed to the right. The rear half is the bed itself,
+          with a carved door cover, draped belt and pillows, and a carved and
+          painted screen with expanded panels around three sides of the bed.
+          <br />
+          The treadle bed is not only warm in winter and cool in summer, but
+          also creates a versatile and private living space in the interior.
+          Inside and outside the bed is like a pavilion, and there is a dressing
+          table, a snack box, a stationery box and a toilet and other household
+          utensils, with carvings and paintings around the bed and layers of
+          pavilion hangings outside the bed, with gold and vermilion lacquer.
+        </div>
+      </div>
+      <div class="craft-row-item">
+        <img src="~/assets/images/朱金漆木雕/5.1.png" class="w-full" />
+        <img src="~/assets/images/朱金漆木雕/5.2.png" class="w-full" />
+        <div class="image-description">Title: Thousand Worker's Bed</div>
+      </div>
+    </div>
+  </div>
+  <div class="gold-row">
+    <div class="container craft-row">
+      <div class="craft-row-item">
+        <img src="~/assets/images/朱金漆木雕/6.png" class="w-full" />
+        <div class="image-description">
+          Title:Ten Thousand Worker's Sedan Chair
+        </div>
+      </div>
+      <div class="craft-row-item">
+        <div class="gold-row-body">
+          "From the Southern Song Dynasty onwards, the folklore of Ningbo girls
+          getting married in sedan chairs became more and more popular, and the
+          sedan chairs became more and more elaborate. The palanquin now in
+          Ningbo has a seven-storey pavilion and five main pavilions, which is
+          called "Wuyue Chaotian".
+
+          <br />
+          It is decorated with vermilion gold carvings, and then lined with
+          embroidery, beads, tassels and lenses. Hundreds of small figures are
+          also carved on the palanquin, so the palanquin is also known as the
+          'Hundred Children's Palanquin'. The palanquin is made of wood, with a
+          vermilion lacquer base, gold foil appliqué, laminated with lenses, and
+          is exquisite and transparent, with its structure symmetrical from
+          front to back, left to right, fine and elaborate, with the top divided
+          into seven layers. The palanquin reflects auspicious themes and
+          historical figures such as the Heavenly Palace bestowing blessings,
+          the unicorn sending a son, the Kui Xing pointing the scholar, and Wang
+          Xizhi's love of geese.
+        </div>
+      </div>
+    </div>
+  </div>
   <ViewProcess></ViewProcess>
 </template>
 
 <style lang="less">
 @import '~/assets/style/common.less';
+.gold-row {
+  padding: 30px 0;
+}
+
+.gold-row-background {
+  background: #ded5c8;
+}
+
+.image-description {
+  .font-RobotoSlab;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  text-align: center;
+  color: #454033;
+}
+
+.gold-row-title {
+  .font-BigShouldersDisplay;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 43px;
+  color: #454033;
+}
+
+.gold-row-body {
+  .font-RobotoSlab;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  color: #454033;
+}
+
+.gold-headline-left {
+  .font-BigShouldersStencilDisplay;
+  font-weight: 700;
+  font-size: 128px;
+  line-height: 134px;
+  letter-spacing: -0.025em;
+  color: #ffffff;
+}
+
+.gold-headline-right {
+  .gold-row-body;
+}
 </style>
