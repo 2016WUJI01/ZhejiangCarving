8bfcd57fffa867e64d052ba0d0003a1b8ab376e2
2016WUJI01 <jiashunwu2001@163.com>
2022-09-07 13:51:00 +0800

    update

--- pages/Craft/mud.vue
+++ pages/Craft/mud.vue
@@ -1,8 +1,230 @@
 <template>
-  <div>mud</div>
+  <div><img src="~/assets/images/泥金彩漆/1.png" class="w-full" /></div>
+  <div>
+    <div class="mud-row mud-row-background">
+      <div class="container craft-row">
+        <div class="craft-row-item mud-headline-left">Mud Gold Lacquerware</div>
+        <div class="craft-row-item mud-headline-right">
+          A unique traditional craft that is on the verge of being lost in China
+        </div>
+      </div>
+    </div>
+    <div class="mud-row">
+      <div class="container craft-row">
+        <div class="craft-row-item">
+          <img src="~/assets/images/泥金彩漆/2.png" class="w-full" />
+        </div>
+        <div class="craft-row-item">
+          <div class="mud-row-title">A brief description is as follows</div>
+          <div class="mud-row-body">
+            Mud gold lacquer is mainly in the wood also pieces, will be beaten
+            into thin pieces of gold and silver foil grinded into powder, mixed
+            into the lacquer material or painted or traced or filled, and the
+            use of clam shell pieces, lenses, sand will, leaching powder and
+            other craft techniques carefully decorated; there are also bamboo
+            pieces; bamboo weave for the tire.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mud-row">
+      <div class="container craft-row">
+        <div class="craft-row-item">
+          <div class="mud-row-title">Historical origins</div>
+          <div class="mud-row-body">
+            You can't see the silver screens woven into the palaces and palaces
+            of today's kings, decorated with pearls and mica, with the seven
+            treasures of hardware.
+            <br />
+            Ningbo has a long history of lacquerware craftsmanship. This is
+            evidenced by a wooden bowl painted with vermilion lacquer that was
+            unearthed at the 7,000-year-old Hemudu site.
+            <br />
+            In the ancient times in Ningbo, country houses often stocked up on
+            large quantities of wood, ready to be put to use when marrying their
+            daughters or brides. The parents of the man's family would make a
+            large wooden sleeping bed for their son before marrying the
+            daughter-in-law; the woman's family would make the red dress
+            furniture. Both families make bamboo and wooden utensils, and when
+            they are finished, they are decorated. In order to make the exterior
+            of the utensils colourful, the craft method of mud and gold lacquer
+            is often chosen, i.e. stacking, leaching and mud and gold painting
+            on the wooden lacquer embryo.
+          </div>
+        </div>
+        <div class="craft-row-item">
+          <img src="~/assets/images/泥金彩漆/3.png" class="w-full" />
+        </div>
+      </div>
+    </div>
+    <div class="mud-row">
+      <div class="container craft-row">
+        <div class="mud-row-title">Reasons for Circulation</div>
+        <div class="mud-row-body">
+          First, the geographical location is superior. Ningbo is located in the
+          southeast coast, north of the Ning-Shao plain, southwest of the
+          semi-hilly area, rich in forestry resources, the early opening of
+          foreign ports, one of the earliest "five ports" of commerce, frequent
+          overseas trade, exchange is very wide, conducive to the development of
+          Yong-style furniture to the outside world, rich in local
+          characteristics of the clay gold lacquer ware favoured by domestic and
+          foreign merchants, which is the formation of This is one of the
+          reasons for the splendour of mud and gold lacquerware.
+        </div>
+      </div>
+    </div>
+    <div class="mud-row">
+      <div class="container craft-row">
+        <div class="craft-row-item">
+          <img src="~/assets/images/泥金彩漆/4.png" class="w-full" />
+          <div class="mud-row-title">Title: Milk Bucket</div>
+          <div class="mud-row-body">
+            The purpose of collecting milk is to suckle a child, and to collect
+            milk also means to "collect a child", to have a child. The addition
+            of a "milk bucket" to a woman's dowry is an auspicious sign that she
+            will have a son.
+          </div>
+        </div>
+        <div class="craft-row-item">
+          <div class="mud-row-body">
+            Secondly, there is a deep cultural deposit. Ningbo has thousands of
+            years of Hemudu culture, plus a long, far-reaching and solid wedding
+            culture, in order to show that the family is rich and wealthy, men
+            and women in the wedding to make a big deal, compared with each
+            other, show off each other, is the ordinary people also want to pour
+            all their money to buy a dowry, in order to boast in the neighbours
+            and show the grand wedding of their children, so that greatly
+            promote the market demand for Yong-style wedding furniture. Ninghai,
+            located in the south of Ningbo, is known as "seven mountains, two
+            waters, one field", rich in human resources, so the history of
+            manual "five artisans" is more developed, in the spare time of
+            farming, the vast majority of master craftsmen with apprentices to
+            travel north and south to do dowry for people, a small scale of The
+            workshops transported the finished products to Taizhou, Ningbo and
+            other areas for sale, and slowly expanded to the surrounding areas
+            such as Ning, Shao and Tai, so that the art of mud and gold lacquer
+            was brought into full play in the wedding furniture, forming a
+            wonder in the category of practical furniture decoration.
+            <br />
+            Thirdly, it has a wide range of applications. It can be widely used
+            as a surface decoration for domestic buildings and everyday
+            utensils, and as a display for artworks in halls. The choice of
+            subject matter is flexible and the plasticity of the art is strong,
+            whether it is historical figures, fish, insects, birds, animals or
+            flowers, whether it is a group or a single pile, regardless of the
+            complexity or simplicity of the shape of the object, it can be
+            adapted to the local situation and played at will. The longevity and
+            durability of the work, which has endured for thousands of years, is
+            evidenced by the clay and gold lacquer of the Shokura-in Temple in
+            Japan.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mud-row">
+      <div class="container craft-row">
+        <div class="craft-row-item">
+          <img src="~/assets/images/泥金彩漆/5.1.png" class="w-full" />
+          <div class="mud-row-title">
+            Title: Wooden Bucket with Clay and Gold Paint
+          </div>
+          <div class="mud-row-body">
+            What is this bucket used for? It was used on three days when I went
+            back to my mother's house, usually to put fried flour ah put cake
+            used, as now women carry LV bags. "When you go back to your mother's
+            house, you fling it around a few times, it's shiny and very
+            elegant."
+          </div>
+        </div>
+        <div class="craft-row-item">
+          <div class="mud-row-title">The role of artifacts</div>
+          <div class="mud-row-body">
+            <div class="craft-row">
+              <div class="craft-row-item">
+                Because of their glorious and colourful appearance, they add a
+                warm, festive and auspicious atmosphere to the owner's home, and
+                because of their clear and three-dimensional decoration,
+                including pavilions and pavilions, auspicious patterns and opera
+                characters, they are not easily worn out and are durable, so
+                they were very popular in the old days in the Ningbo
+                countryside.
+              </div>
+              <div class="craft-row-item">
+                <img
+                  src="~/assets/images/泥金彩漆/5.2.png"
+                  class="w-full"
+                  style="border-radius: 50%"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="mud-row-body">
+            It has been proven that in Ninghai, in ancient times, the rich and
+            well-off countryside families often chose mud and gold lacquer
+            furniture and supplies when men married women, and this became a
+            custom over time: before the man's parents married their
+            daughter-in-law, they had to make a large wooden mud and gold
+            lacquer bed for their son; the woman's family had to buy festive mud
+            and gold lacquer furniture, such as boxes, baskets and hat buckets,
+            and so on.
+            <br />
+            Mud and gold lacquer is closely related to the life of the local
+            people. There is a wide variety of lacquer, ranging from furniture
+            such as sleeping beds and cupboards to household utensils such as
+            buckets, fruit boxes and hat buckets, reflecting the habits and
+            customs of the people. The Ming and Qing dynasties and the
+            Republican period furniture and household utensils preserved in
+            Ningbo today are as bright as new, showing the exquisite handcrafted
+            skills of the time.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <ViewProcess></ViewProcess>
 </template>
 
 <style lang="less">
 @import '~/assets/style/common.less';
+.mud-row {
+  padding: 30px 0;
+}
+.mud-row-background {
+  background: #d1bab5;
+}
+
+.image-description {
+  .font-RobotoSlab;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  text-align: center;
+  color: #454033;
+}
+.mud-row-title {
+  .font-BigShouldersDisplay;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 43px;
+  color: #454033;
+}
+.mud-row-body {
+  .font-RobotoSlab;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  color: #454033;
+}
+.mud-headline-left {
+  .font-BigShouldersStencilDisplay;
+  font-weight: 700;
+  font-size: 128px;
+  line-height: 134px;
+  letter-spacing: -0.025em;
+  color: #fff;
+}
+.mud-headline-right {
+  .font-RobotoSlab;
+  .mud-row-body;
+}
 </style>
