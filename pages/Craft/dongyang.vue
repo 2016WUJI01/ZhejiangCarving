255b4a0b09d2e360868ec5a55b287416614bc530
2016WUJI01 <jiashunwu2001@163.com>
2022-09-07 14:07:32 +0800

    update

--- pages/Craft/dongyang.vue
+++ pages/Craft/dongyang.vue
@@ -1,8 +1,234 @@
 <template>
-  <div>dongYang</div>
+  <div><img src="~/assets/images/东阳木雕/1.png" class="w-full" /></div>
+  <div>
+    <div class="dongyang-row dongyang-row-background">
+      <div class="container craft-row">
+        <div class="craft-row-item dongyang-headline-left">
+          DongYang Wood Carving
+        </div>
+        <div class="craft-row-item dongyang-headline-right">
+          A thousand-year tradition of ingenuity on wood
+        </div>
+      </div>
+    </div>
+    <div class="dongyang-row">
+      <div class="container craft-row">
+        <div class="dongyang-row-title">A brief description is as follows</div>
+        <div class="craft-row-item">
+          <img src="~/assets/images/东阳木雕/2.png" class="w-full" />
+        </div>
+        <div class="craft-row-item">
+          <div class="dongyang-row-body">
+            Dongyang wood carving originated in Dongyang, Zhejiang Province, and
+            has a history of over a thousand years since the Tang Dynasty,
+            making it an excellent representative of Chinese folk carving art.
+            In 2006, Dongyang wood carving was approved by the State Council to
+            be included in the first batch of national intangible cultural
+            heritage list. From the earliest production of wooden figurines, to
+            architectural decoration of palaces, temples and houses, to modern
+            ornaments, furniture, wall hangings and interior design, etc., this
+            art has evolved and innovated through the millennia, showing great
+            vitality under the inheritance and promotion of generations of wood
+            carving artists. By the end of 2019, Dongyang had three Asia Pacific
+            handicraft masters, more than 50 national and provincial arts and
+            crafts masters, and over 3,000 professionals of all kinds.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dongyang-row">
+      <div class="container craft-row">
+        <div class="dongyang-row-title">Production process</div>
+        <div class="dongyang-row-body">
+          Dongyang wood carving is a planar relief carving with a scattered
+          perspective, and its carving materials are widely used, because of the
+          fine carving, generally less lacquer, highlighting the original taste
+          of the original wood, also known as "white wood carving". The
+          materials used in Dongyang wood carving are mainly divided into: the
+          content of the subject matter carved with materials and the border
+          with materials. The content of the carving material requires delicate
+          wood grain, durable, non-greasy, not easy to deformation, not easy to
+          crack, such as camphor, linden, poplar, birch, white wood, white
+          mahogany, ginkgo wood, nan wood, beech wood and other light-coloured
+          wood, suitable for the fine carving of Dongyang wood carving. For the
+          border material, the wood is required to be tough, hard and heavy. It
+          is not easily damaged and plays a reinforcing and backing role. Such
+          as ebony, sourwood, mahogany, pearwood, iron pearwood, etc. The
+          materials chosen should make use of the natural colour and texture of
+          the wood, so that the structural content of the work is integrated
+          with the outer frame shape. The colour and texture of the wood carving
+          material is the "skin" of wood carving, wood carving works after
+          carving, giving the language of carving, its material gives a certain
+          material language, can deepen the infectious power of wood carving
+          art.
+          <br />
+          In recent years, Dongyang wood carving in the choice of carving
+          materials, whether it is the border, or the main carving part, all use
+          high-grade materials for carving production, such as the use of
+          small-leaf rosewood, large-leaf rosewood, sourwood, pearwood,
+          golden-wood, red bean fir and other high-grade wood for carving
+          production. The aim is to enhance the economic value of wood carvings
+          through the carving of high-grade materials. However, as far as the
+          artistic expression of fine carving is concerned, there is no effect
+          of lighter colours.
+        </div>
+      </div>
+    </div>
+    <div class="dongyang-row">
+      <div class="container craft-row">
+        <div class="craft-row-item">
+          <img src="~/assets/images/东阳木雕/3.1.png" class="w-full" />
+        </div>
+        <div class="craft-row-item">
+          <img src="~/assets/images/东阳木雕/3.2.png" class="w-full" />
+        </div>
+      </div>
+    </div>
+    <div class="dongyang-row">
+      <div class="container craft-row">
+        <div class="dongyang-row-title">Wood carving composition</div>
+        <div class="dongyang-row-body">
+          The composition of a Dongyang wood carving is the author's idea and
+          intention to express the theme of thought through the reasonable
+          arrangement of graphics to form a complete picture, to achieve the
+          expression of the mood, the content of the work of art. The
+          composition is the skeleton of the entire work of Dongyang wood
+          carving, and is the carrier of the intention. The composition should
+          include the mood, content and method of expression, which has an
+          important relationship with the success of the creation of the work
+          and the overall artistic effect. In the composition, the part of the
+          theme that can attract the viewer's attention should be fully
+          expressed, and the accompanying objects should be handled in a general
+          way, so that the accompanying objects can play a role in the artistic
+          effect. The work of Dongyang wood carving is to choose things that one
+          is more familiar with and feels more deeply about, and to express
+          one's thoughts and emotions and thematic content, as well as one's
+          opinions on art, fully through the vehicle of wood carving.
+          <br />
+          Dongyang wood carvings are full of composition, rich in expressive
+          content, well delineated and layered, and appropriately reserved for
+          flat surfaces, with the artistic effect of viewing the big picture in
+          a small way. Within a certain plane and space, it can express a richer
+          content than Western relief carvings. The composition of Dongyang wood
+          carvings can be summarised as a scattered perspective approach to
+          composition, which is based on several aspects such as the combination
+          of line and surface modelling techniques, the appropriate retention of
+          flat surfaces, small pieces of reserved land and the high and far
+          overlapping layers of treatment.
+          <br />
+          The composition of Dongyang wood carvings is characterised by a high,
+          distant, flat and scattered level of perspective and by the
+          traditional Chinese painting of scattered perspective or bird's-eye
+          perspective, which can be richer than that of Western relief carvings,
+          and can be free from the "large near and small far", "scene clear "It
+          is not bound by the rules of Western carving and painting, such as
+          'large near and small far', 'clear view' and 'distant view', but fully
+          displays the content of the picture, which can be described as
+          'painting within painting, view within view'. The artwork is logical,
+          clear, thematic, pleasing to the eye and charming.
+        </div>
+      </div>
+    </div>
+    <div><img src="~/assets/images/东阳木雕/4.png" class="w-full" /></div>
+    <div class="dongyang-row">
+      <div class="container craft-row">
+        <div class="craft-row-item">
+          <div class="dongyang-row-title">Artistic expression</div>
+          <div class="dongyang-row-body">
+            With its rich subject matter and excellent carving skills, Dongyang
+            wood carving has gradually developed the distinctive effect of flat
+            relief art. The artistic expression of Dongyang wood carving is
+            characterised by the use of carving techniques, carving materials,
+            colours and paste techniques. (i) Artistic expression using carving
+            techniques. (ii) The artistic expression of the use of carving
+            materials. (iii) The artistic expression of the use of colour
+            treatment. (d) Artistic expression using pasting techniques.
+          </div>
+        </div>
+        <div class="craft-row-item">
+          <img src="~/assets/images/东阳木雕/5.png" class="w-full" />
+        </div>
+      </div>
+    </div>
+    <div class="dongyang-row">
+      <div class="container craft-row">
+        <div class="craft-row-item"></div>
+        <div class="craft-row-item">
+          <div class="dongyang-row-title">Works on the subject</div>
+        </div>
+      </div>
+    </div>
+    <div class="dongyang-row">
+      <div class="container craft-row">
+        <div class="craft-row-item">
+          <img src="~/assets/images/东阳木雕/6.png" class="w-full" />
+          <div class="image-description">Cicadas carved on wood as shown</div>
+        </div>
+        <div class="craft-row-item">
+          <div class="dongyang-row-body">
+            The types of wood carvings in Dongyang that are characteristic of
+            the content of the subject matter are: figures, landscapes, flowers
+            and birds, etc. The main contents are: historical events, myths and
+            legends, folk tales, classical literature, auspicious animals,
+            sentimental flowers and trees, folkloric figures, abstract patterns,
+            landscape figures, fish, insects and animals, religious Buddha
+            statues, etc. The symbolic and allegorical meaning of the works is
+            to use animals and flowers to express auspiciousness and happiness,
+            longevity and love between husband and wife, and to use mythological
+            stories and historical figures with strength of character that are
+            celebrated, worshipped and respected by the people to express their
+            respect and affection for the past and the present.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <ViewProcess></ViewProcess>
 </template>
 
 <style lang="less">
 @import '~/assets/style/common.less';
+.dongyang-row {
+  padding: 30px 0;
+}
+.dongyang-row-background {
+  background: #d1bab5;
+}
+
+.image-description {
+  .font-RobotoSlab;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  text-align: center;
+  color: #454033;
+}
+.dongyang-row-title {
+  width: 100%;
+  .font-BigShouldersDisplay;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 43px;
+  color: #454033;
+}
+.dongyang-row-body {
+  width: 100%;
+  .font-RobotoSlab;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  color: #454033;
+}
+.dongyang-headline-left {
+  .font-BigShouldersStencilDisplay;
+  font-weight: 700;
+  font-size: 128px;
+  line-height: 134px;
+  letter-spacing: -0.025em;
+  color: #fff;
+}
+.dongyang-headline-right {
+  .font-RobotoSlab;
+  .dongyang-row-body;
+}
 </style>
