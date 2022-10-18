6eff0b5bbc4bbb6d585ba4ddeffe70ecaa1ad4f8
2016WUJI01 <jiashunwu2001@163.com>
2022-10-18 12:23:52 +0800

    update

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-module.exports = {
-  semi: false,
-  singleQuote: true,
-  endOfLine: "lf",
-  tabWidth: 2,
-  jsxBracketSameLine: true,
-  trailingComma: "es5",
-};
